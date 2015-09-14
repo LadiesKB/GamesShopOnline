3d56394ccf426567399c0da28ec5a899006e3c56
tsydik <v.tsydik@gmail.ru>
2015-09-14 16:39:09 +0300

    Создали основные сущности

---
+++ GameShopOnline/GameShopOnline/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameShopOnline
+{
+    class Game : BaseModel
+    {
+        public string Name { get; set; }
+        
+    }
+}
---
+++ GameShopOnline/GameShopOnline/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameShopOnline
+{
+    class Genre : BaseModel
+    {
+        public string GenreOfGame { get; set; }
+    }
+}
